7425e000832657a639e94d1b7f0a7d9b9aa9cca1
tomislavandro <tcanic@pmfst.hr>
2025-01-07 12:58:25 +0100

    Create App.jsx
    main stranica u reactu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Categories />} />
+        </Routes>
+      </Router>
+  )
+}
