90515da557bc967ca9ea63ae2f54130beafbffe0
tomislavandro <tcanic@pmfst.hr>
2025-01-08 14:25:13 +0100

    Create Categories.jsx

---
+++ frontend/src/Categories.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import "./styles/categories.css";
+
+const Categories = () => {
+    return (
+        <div className="categories-container">
+            <h2>Kategorije</h2>
+              <div className="category-buttons">
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
