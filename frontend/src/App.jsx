baf36a63b3f6ffd9fd54b2a94a211d185b735e3f
tomislavandro <tcanic@pmfst.hr>
2025-01-11 10:31:03 +0100

    Update App.jsx
    dodani kolacici za login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Cookies from "js-cookie";
 import './App.css';
+import Navbar from './components/NavBar.jsx';
+import Login from './components/Login.jsx';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => { // NOVO: Provjeri kolačiće prilikom učitavanja
+    const userCookie = Cookies.get('user');
+    if (userCookie) {
+      setIsLoggedIn(true); // NOVO: Ako kolačić postoji, postavi stanje na prijavljen
+    }
+  }, []);
+
+  
   return (
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Categories />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
   )
