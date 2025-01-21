a905ad1d20b4f5fd4e051f94e6b1d77d0c95a587
lsrhoj <lsrhoj@pmfst.hr>
2025-01-21 15:16:21 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/navbar.css";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-content">
-                <Link to="/" className="navbar-item">Početna stranica</Link>
-                <div className="navbar-right">
+                <div className="navbar-left">
+                    <Link to="/" className="navbar-item">Početna stranica</Link>
                     <Link to="/login" className="navbar-item">Prijava</Link>
-                    <Link to="/profile" className="navbar-item">👤</Link>
+                    <Link to="/register" className="navbar-item">Registracija</Link>
+                </div>
+                <div className="navbar-right">
+                    
+                    <Link to="/profile" className="navbar-item profile-icon">👤</Link>
                 </div>
             </div>
         </nav>
