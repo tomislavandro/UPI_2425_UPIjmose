f8af847826c8aed2c44448eb9e5db4e6f987ea70
denipanda <ivan.deni1501@gmail.com>
2025-01-21 15:56:24 +0100

    Update App.jsx
    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cookies from "js-cookie";
 import './App.css';
+import Categories from './Categories.jsx';
 import Navbar from './components/NavBar.jsx';
 import Login from './components/Login.jsx';
+import Register from "./components/Signup.jsx"
+import Profile from './components/Profile.jsx';
+import Reviews from './components/CategoryReview.jsx';
+
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // NOVO: Stanje za prijavu
 
   useEffect(() => { // NOVO: Provjeri kolačiće prilikom učitavanja
     const userCookie = Cookies.get('user');
@@ -15,16 +21,23 @@ function App() {
     }
   }, []);
 
-  
+
   return (
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Categories />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Categories />} />
+        <Route path="/reviews" element={<h1>Recenzije</h1>} /> {/* Ovo možeš zamijeniti s komponentom za recenzije */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/reviews/:categoryId" element={<Reviews/>} />
+
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
+
