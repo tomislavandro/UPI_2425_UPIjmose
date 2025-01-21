11936c54d6800b09d9b8f5750b9d7073c6ddc62b
azupan129 <azupan@pmfst.hr>
2025-01-21 15:08:15 +0100

    Update category.model.js

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const categorySchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String },
-    locations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Location' }]
+    locations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Location' }],
+    image: { type: String }
 });
 
 const Category = mongoose.model("Category", categorySchema);
