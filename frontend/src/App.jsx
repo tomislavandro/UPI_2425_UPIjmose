7fc122d6dc1c7f98e09f7c73824448e68fbc4d26
azupan129 <azupan@pmfst.hr>
2025-01-26 13:33:01 +0100

    Ažuriran App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useLocation } from 'react-router-dom'; 
 import Cookies from "js-cookie";
 import './App.css';
 import Categories from './Categories.jsx';
@@ -9,21 +10,19 @@ import Register from "./components/Signup.jsx"
 import Profile from './components/Profile.jsx';
 import Reviews from './components/CategoryReview.jsx';
 
-
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // NOVO: Stanje za prijavu
-
-  useEffect(() => { // NOVO: Provjeri kolačiće prilikom učitavanja
+  useEffect(() => {
     const userCookie = Cookies.get('user');
     if (userCookie) {
-      setIsLoggedIn(true); // NOVO: Ako kolačić postoji, postavi stanje na prijavljen
+      setIsLoggedIn(true);
     }
   }, []);
 
-
   return (
     <Router>
+      <BackgroundWrapper>
       <Navbar />
       <Routes>
         <Route path="/" element={<Categories />} />
@@ -31,13 +30,22 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/reviews/:categoryId" element={<Reviews/>} />
-
+        <Route path="/reviews/:categoryId" element={<Reviews />} />
       </Routes>
+      </BackgroundWrapper>
     </Router>
   );
 }
 
+function BackgroundWrapper({ children }) {
+  const location = useLocation();
+  const hasBackground = location.pathname === "/login" || location.pathname === "/register";
+
+  return (
+    <div className={hasBackground ? "with-background" : "without-background"}>
+      {children}
+    </div>
+  );
+}
+
 export default App;
-
-
