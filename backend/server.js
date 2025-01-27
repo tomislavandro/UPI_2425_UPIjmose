4f508ae26085662e1e49cd4ed2ca5a0fff2c2b70
tomislavandro <tcanic@pmfst.hr>
2025-01-27 19:29:05 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 1000
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: "*",
     credentials: true
 }));
 app.use(cookieParser());
